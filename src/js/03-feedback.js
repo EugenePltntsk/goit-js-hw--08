b453c8e6f3c2846618dfeaf601d6fc325f6b843e
Eugene Plotnytskyi <eugeneplotnytskyi@gmail.com>
2022-09-23 10:29:20 +0300

    done hw-js-08-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle';
+
+const key = 'feedback-form-state';
+const inputEl = document.querySelector('.feedback-form input');
+// console.log(inputEl);
+
+const textAreaEl = document.querySelector('.feedback-form textarea');
+// console.log(textAreaEl);
+
+const formEl = document.querySelector('.feedback-form');
+// console.log(formEl);
+
+
+
+const onChange = event => {
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+
+  const infoObj = {
+    email,
+    message,
+  };
+  console.log(infoObj);
+
+  localStorage.setItem(key, JSON.stringify(infoObj));
+};
+
+formEl.addEventListener('input', throttle(onChange, 500));
+
+
+
+
+const checkFoo = () => {
+  const saveData = localStorage.getItem(key);
+
+  const parsedData = JSON.parse(saveData);
+  if (parsedData) {
+    inputEl.value = parsedData.email;
+    textAreaEl.value = parsedData.message;
+  }
+};
+
+checkFoo();
+
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event)  {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem(key);
+};
+
+
 
